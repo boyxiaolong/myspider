14ff710f9cb269a6e403da245caebc1c986d5440
boyxiaolong <boyhailong@gmai.com>
2016-04-01 20:42:21 +0800

    simplify

--- geventspider.py
+++ geventspider.py
@@ -5,97 +5,50 @@ from gevent import (monkey,
                     event,
                     pool)
 
-import re
-import sys
-import logging
-import urllib
-import urlparse
 import requests
 from threading import Timer
-from pyquery import PyQuery
-from utils import HtmlAnalyzer, UrlFilter
-
+from utils import HtmlAnalyzer
 
 class Strategy(object):
-    def __init__(self,max_depth=5, max_count=5000, concurrency=5):
+    def __init__(self,max_depth, max_count, concurrency=10):
         self.max_depth = max_depth
         self.max_count = max_count
         self.concurrency = concurrency
-        self.timeout = 10
-        self.time = 6*3600
+        self.timeout = 60
+        self.time = 12*3600
 
 class UrlObj(object):
-    def __init__(self, url, depth=0, linkin=None):
+    def __init__(self, url, depth):
         self.url = url.strip('/')
         self.depth = depth
-        self.linkin = linkin
-
-    def __hash__(self):
-        return hash(self.url)
-
-
-class UrlTable(object):
-    infinite = float("inf")
-
-    def __init__(self, size=0):
-        self.__urls = {}
-
-        if size == 0 :
-            size = self.infinite
-        self.size = size
-
-    def __len__(self):
-        return len(self.__urls)
-
-    def __contains__(self, url):
-        return hash(url) in self.__urls.keys()
-
-    def __iter__(self):
-        for url in self.urls:
-            yield url
-
-    def insert(self, url):
-        if isinstance(url, basestring):
-            url = UrlObj(url)
-        if url not in self:
-            self.__urls.setdefault(hash(url), url)
-
-    @property
-    def urls(self):
-        return self.__urls.values()
-
-    def full(self):
-        return len(self) >= self.size
-
 
 class GeventSpider(object):
     def __init__(self, max_depth, max_count, root_url):
         monkey.patch_all()
-        print max_depth, max_count
         self.strategy = Strategy(max_depth, max_count)
         self.queue = queue.Queue()
-        self.urltable = UrlTable(self.strategy.max_count)
         self.pool = pool.Pool(self.strategy.concurrency)
         self.greenlet_finished = event.Event()
         self._stop = event.Event()
-        self.setRootUrl(root_url)
+        self.url_set = set()
+        obj = UrlObj(root_url, 0)
+        self.put(obj)
+        self.url_num = 0
 
-    def setRootUrl(self,url):
-        if isinstance(url, basestring):
-            url = UrlObj(url)
-        self.root = url
-        self.put(self.root)
-
-    def put(self, url):
-        if url not in self.urltable:
-            self.queue.put(url)
+    def put(self, obj):
+        if obj.url not in self.url_set:
+            self.url_set.add(obj.url)
+            self.queue.put(obj)
 
     def run(self):
         self.timer = Timer(self.strategy.time, self.stop)
         self.timer.start()
-        print ("spider '%s' begin running" % self.root)
 
         while not self.stopped() and self.timer.isAlive():
+            if self.url_num >= self.strategy.max_count:
+                print 'need stop'
+                self.stop()
+                return
             for greenlet in list(self.pool):
                 if greenlet.dead:
                     self.pool.discard(greenlet)
@@ -110,6 +63,7 @@ class GeventSpider(object):
                     self.stop()
             greenlet = Handler(url, self)
             self.pool.start(greenlet)
+            self.url_num = self.url_num+1
 
     def stopped(self):
         return self._stop.is_set()
@@ -120,16 +74,6 @@ class GeventSpider(object):
         self.pool.join()
         self.queue.put(StopIteration)
         return
-
-    def dump(self):
-        import StringIO
-        out = StringIO.StringIO()
-        for url in self.urltable:
-            try:
-                print >> out ,url
-            except:
-                continue
-        return out.getvalue()
 
 
 class Handler(gevent.Greenlet):
@@ -142,32 +86,23 @@ class Handler(gevent.Greenlet):
 
     def _run(self):
         strategy = self.spider.strategy
-        urltable = self.spider.urltable
-        queue = self.spider.queue
+        urltable = self.spider.url_set
 
         try:
             html = self.open(self.urlobj.url)
         except Exception, why:
             return self.stop()
 
-        linkin = self.urlobj
-        depth = linkin.depth + 1
+        depth = self.urlobj.depth + 1
 
         if strategy.max_depth and (depth > strategy.max_depth):
             return self.stop()
 
         for link in self.feed(html):
-            if urltable.full():
-                self.stop()
-                self.spider.stop()
-                return
-
             if link in urltable:
                 continue
-
-            url = UrlObj(link, depth, linkin)
-            urltable.insert(url)
-            queue.put(url)
+            url = UrlObj(link, depth)
+            self.spider.put(url)
 
         self.stop()
 
@@ -188,7 +123,6 @@ class Handler(gevent.Greenlet):
     def feed(self,html):
         return HtmlAnalyzer.extractLinks(html,self.urlobj.url,self.charset)
 
-
     def stop(self):
         self.spider.greenlet_finished.set()
         self.kill(block=False)
@@ -201,5 +135,5 @@ class MySpider(object):
     def run(self):
         self.spider.run()
 
-test = MySpider(max_depth=10, max_count=5000, root_url="http://www.douban.com")
+test = MySpider(max_depth=20, max_count=30, root_url="http://www.douban.com")
 test.run()
